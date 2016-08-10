1acca8deeee5b9a4a0b3e2ab4fae8672c032543d
marco-bmz <m.keppa@bmz-dueren.de>
2016-08-10 13:47:45 +0200

    bmz-test-folder

---
+++ index.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="de">
+<head><meta charset="UTF-8">
+
+<title>Bau-Medien-Zentrum</title>
+<meta name="keywords" content="bau medien zentrum, axel leroy, dueren, bauen, lernen">
+
+<meta name="Description" content="Bau-Medien-Zentrum Düren">
+
+<meta name="robots" content="all, index, follow">
+<meta name="revisit-after" content="14 days">
+<meta name="date" content="2016-07-14">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.4, user-scalable=yes">
+
+
+<link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/icons/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/icons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/icons/manifest.json">
+<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/icons/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/icons/mstile-144x144.png">
+<meta name="msapplication-config" content="/icons/browserconfig.xml">
+<meta name="theme-color" content="#e3cc00">
+
+<meta name="geo.region" content="DE" />
+<meta name="geo.placename" content="D&uuml;ren" />
+<meta name="geo.position" content="50.804;6.45304" />
+<meta name="ICBM" content="50.804, 6.45304" />
+
+
+<style type="text/css">
+    <?php include "incl/style-min.css";?>
+    <?php include "incl/style.css";?>
+</style>
+
+<!--
+<link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet' type='text/css'>
+ -->
+    
+    
+</head>
+
+<body id="openNav"><span id="seite1">
+
+<header id="head" class="sticky">
+    
+    <!--
+    <a href="index.php" title="zurück zur Homepage">
+    <picture alt="">
+        <source media="(min-width: 750px)" srcset="home/pic/header-logo-300.png" type="image/png" title="">
+        <img src="home/pic/header-logo-150.png" width="150" height="150" alt="" title="">
+    </picture>
+    </a>
+    -->
+    
+<!-- NAVIGATION ANFANG--><!-- NAVIGATION ANFANG-->
+<!-- NAVIGATION ANFANG--><!-- NAVIGATION ANFANG-->
+
+<!-- Hier beginnt der Bereich für die Menüpunkte -->
+  <nav class="navMain">
+  <article id="navi">
+
+    <a href="index.php" title="" class="seite1"><section>Home</section></a>
+    <a href="#" title="" class="seite2"><section>Ausstellung</section></a>
+    <a href="#" title="" class="seite3"><section>Seminare</section></a>
+    <a href="#" title="" class="seite4"><section>Wissen</section></a>
+    <a href="#" title="" class="seite5"><section>Imagine Rooms</section></a>
+
+  </article>
+  </nav>
+<!-- Hier endet der Bereich für die Menüpunkte -->
+
+<!-- Hier beginnt der Bereich für den Menübutton -->
+  <div id="content">
+    <a href="#openNav" title="Hier ist die ganze Navigation der Seite" id="openBtn">&#x2261;</a><!---->
+    <a href="#closeNav" title="Hier kann die Navigation geschlossen werden" id="closeBtn">X</a><!--&#x2261;-->
+  </div>
+<!-- Hier endet der Bereich für den Menübutton -->
+
+<!-- NAVIGATION ENDE--><!-- NAVIGATION ENDE-->
+<!-- NAVIGATION ENDE--><!-- NAVIGATION ENDE-->
+    
+    <section class="header">
+        Bau-Medien-Zentrum
+    </section>
+</header>
+
+
+<section id="main">
+    <article class="main1">
+        <style type="text/css">
+    <?php include "incl/style-image.css";?>
+</style>
+        <section>
+            <div class="block image1">
+              <div class="layer"></div>
+              <div class="p-container">
+                 <p class="img-p"><a href="#ausstellung">Die Ausstellung im Bau-Medien-Zentrum Düren</a></p>
+               </div>
+            </div>
+
+            <div class="block image2">
+              <div class="layer"></div>
+              <div class="p-container">
+                 <p class="img-p"><a href="#seminare">Seminare<br>Hier wird Wissen praktisch vermittelt</a></p>
+               </div>
+            </div>
+
+            <div class="block image3">
+              <div class="layer"></div>
+              <div class="p-container">
+                 <p class="img-p"><a href="#wissen">Wissen</a></p>
+               </div>
+            </div>
+            
+            <div class="block image4">
+              <div class="layer"></div>
+              <div class="p-container">
+                 <p class="img-p"><a href="#imagine-rooms">Imagine Rooms</a></p>
+               </div>
+            </div>
+        </section>
+    </article>
+        
+    <article class="main2 content1">
+        <section>
+            <h1 id="ausstellung">Die Ausstellung</h1>
+        </section>
+    </article>
+    <article class="main2">
+        <section>
+            <h1 id="seminare">Seminare</h1>
+        </section>
+    </article>
+    <article class="main2 content1">
+        <section>
+            <h1 id="wissen">Wissen</h1>
+        </section>
+    </article>
+    <article class="main2">
+        <section>
+            <h1 id="imagine-rooms">Imagine Rooms</h1>
+        </section>
+    </article>
+</section>
+
+<footer>
+    <a href="#head" title="Hier klicken &ndash; Und schon geht es zum oberen Ende der Seite"><section class="back-to-top">^<br /><span class="btt-text">Back to Top</span></section></a>
+    <article>
+    <section>
+        <a href="#">Impressum</a>
+    </section>
+    <section>
+        <a href="#">Kontakt</a>
+    </section>
+    <section>
+        <a href="#"></a>
+    </section>
+    <section>
+     
+    </section>
+    </article>
+</footer>
+        <!-- Google Analytics-Code -->
+        <script>
+            (function (window, document, variableName, scriptElement, firstScript) {
+                window['GoogleAnalyticsObject'] = variableName;
+                window[variableName] || (window[variableName] = function () {
+                    (window[variableName].q = window[variableName].q || []).push(arguments);
+                });
+                window[variableName].l = +new Date;
+                scriptElement = document.createElement('script'),
+                    firstScript = document.scripts[0];
+                scriptElement.src = 'https://www.google-analytics.com/analytics.js';
+                firstScript.parentNode.insertBefore(scriptElement, firstScript)
+            }(window, document, 'ga'));
+
+            ga('create', 'UA-');
+            ga('send', 'pageview');
+            ga('set', 'anonymizeIp', true);
+        </script>
+        <!-- End Google Analytics-Code -->
+<script type="text/javascript" id="cookieinfo"
+	src="incl/cookieinfo.min.js">
+</script>
+
+
+</span></body>
+</html>
---
+++ text/bau-medien-zentrum.php
@@ -0,0 +1,11 @@
+<style>
+    .bmz-bau {
+        color: #5e6668
+    }
+    .bmz-medien {
+        color: #01497b
+    }
+    .bmz-zentrum {
+        color: #5b6969
+    }</style>
+<span class="bmz-bau">Bau-</span><span class="bmz-medien">Medien-</span><span class="bmz-zentrum">Zentrum</span>
