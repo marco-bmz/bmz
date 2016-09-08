6ca819ee7d470bb862391a263d057cd4e7ddb50c
marco-bmz <m.keppa@bmz-dueren.de>
2016-09-08 09:25:44 +0200

    new branch
    neuer test branch

---
+++ bmz1/incl/header.php
@@ -0,0 +1,34 @@
+<header id="head" class="sticky">
+    <a href="index.php" title="zurück zur Homepage">
+    <picture alt="">
+        <source media="(min-width: 750px)" srcset="home/pic/header-logo-300.png" type="image/png" title="Das Logo zur Seite www.visitenkarte-online.net - zurück zur Homepage">
+        <img src="home/pic/header-logo-150.png" width="150" height="150" alt="Das Logo zur Seite www.visitenkarte-online.net - zurück zur Homepage" title="Das Logo zur Seite - zurück zur Homepage">
+    </picture>
+    </a>
+<!-- NAVIGATION ANFANG--><!-- NAVIGATION ANFANG-->
+<!-- NAVIGATION ANFANG--><!-- NAVIGATION ANFANG-->
+
+<!-- Hier beginnt der Bereich für die Menüpunkte -->
+  <nav class="navMain">
+  <article id="navi">
+
+    <a href="index.php" title="" class="seite1"><section>Home</section></a>
+    <a href="#" title="" class="seite2"><section>Referenzen</section></a>
+    <a href="#" title="" class="seite3"><section>Preise</section></a>
+    <a href="#" title="" class="seite4"><section>Kontakt</section></a>
+    <a href="#" title="" class="seite5"><section>Impressum</section></a>
+
+  </article>
+  </nav>
+<!-- Hier endet der Bereich für die Menüpunkte -->
+
+<!-- Hier beginnt der Bereich für den Menübutton -->
+  <div id="content">
+    <a href="#openNav" title="Navigation &ouml;ffnen" id="openBtn">&#x2261;</a>
+    <a href="#closeNav" title="Navigation schlie&szlig;en" id="closeBtn">&#x2261;</a>
+  </div>
+<!-- Hier endet der Bereich für den Menübutton -->
+
+<!-- NAVIGATION ENDE--><!-- NAVIGATION ENDE-->
+<!-- NAVIGATION ENDE--><!-- NAVIGATION ENDE-->
+</header>
